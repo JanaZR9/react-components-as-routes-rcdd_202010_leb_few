3d643f1b27a9c142a2faf233550527c09a3245d9
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-13 04:28:32 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -14,7 +14,29 @@ import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 const Navbar = () =>
   <div>
     <NavLink
-      to="/"  exact  style={link}
+      to="/"  exact  style={link} 
+       activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+  
 const Home = () => {
   return (
     <div>
